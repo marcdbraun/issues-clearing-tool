3944cccbbe44bf81c33f9cfc00b1a99875d75311
Marc Braun <marcdbraun@Marcs-MacBook-Pro.local>
2026-02-06 14:49:10 -0600

    Configure custom domain apps.encouragingleaders.com

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/issues-clearing-tool/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
